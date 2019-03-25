8b7ffada251c4f0de4c60a11a348a0ef5c479303
Andreas Robecke <a.robecke@getsafe.de>
2019-03-25 08:58:01 +0100

    Get rid of active support concern

--- lib/nxt_error_registry.rb
+++ lib/nxt_error_registry.rb
@@ -6,47 +6,43 @@ require "nxt_error_registry/codes_harness"
 require 'railtie' if defined?(Rails)
 
 module NxtErrorRegistry
-  extend ActiveSupport::Concern
   RegistrationError = Class.new(StandardError)
   CodeValidator = DefaultCodeValidator
 
+  def register_error(name, type:, code:, **opts, &block)
+    raise_name_not_a_symbol_error(name) unless name.is_a?(Symbol)
+    raise_registration_error(name) if const_defined?(name)
 
-  module ClassMethods
-    def register_error(name, type:, code:, **opts, &block)
-      raise_name_not_a_symbol_error(name) unless name.is_a?(Symbol)
-      raise_registration_error(name) if const_defined?(name)
+    error_class = Class.new(type, &block)
+    error_class.define_singleton_method :code, -> { code }
+    error_class.define_singleton_method :options, -> { opts }
 
-      error_class = Class.new(type, &block)
-      error_class.define_singleton_method :code, -> { code }
-      error_class.define_singleton_method :options, -> { opts }
+    const_set(name, error_class)
+    entry = { code: code, error_class: error_class, type: type, name: name, namespace: self.to_s, opts: opts }
+    error_registry[name.to_s] = entry
 
-      const_set(name, error_class)
-      entry = { code: code, error_class: error_class, type: type, name: name, namespace: self.to_s, opts: opts }
-      error_registry[name.to_s] = entry
+    # This depends on duplicate entries so has to come last
+    validate_code(name, type, code)
+  end
 
-      # This depends on duplicate entries so has to come last
-      validate_code(name, type, code)
+  private
+
+  def validate_code(name, type, code)
+    CodeValidator.new(name, type, code, self).validate
+  end
+
+  def error_registry
+    @error_registry ||= begin
+      ::NxtErrorRegistry::Registry.instance[self.to_s] ||= { }
+      ::NxtErrorRegistry::Registry.instance[self.to_s]
     end
+  end
 
-    private
+  def raise_registration_error(name)
+    raise RegistrationError, "#{name} was already registered in #{self}"
+  end
 
-    def validate_code(name, type, code)
-      CodeValidator.new(name, type, code, self).validate
-    end
-
-    def error_registry
-      @error_registry ||= begin
-        ::NxtErrorRegistry::Registry.instance[self.to_s] ||= { }
-        ::NxtErrorRegistry::Registry.instance[self.to_s]
-      end
-    end
-
-    def raise_registration_error(name)
-      raise RegistrationError, "#{name} was already registered in #{self}"
-    end
-
-    def raise_name_not_a_symbol_error(name)
-      raise ArgumentError, "Error name '#{name}' must be a symbol"
-    end
+  def raise_name_not_a_symbol_error(name)
+    raise ArgumentError, "Error name '#{name}' must be a symbol"
   end
 end
