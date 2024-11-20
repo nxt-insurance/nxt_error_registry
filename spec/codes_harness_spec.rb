d638582c20be99b82947d6381a5a526eb617b817
Sergei Chizhikov <chiibis@yandex.ru>
2024-11-20 12:49:52 +0100

    Split rubocop and rspec in CircleCI

--- spec/codes_harness_spec.rb
+++ spec/codes_harness_spec.rb
@@ -1,14 +1,6 @@
 RSpec.describe NxtErrorRegistry::CodesHarness do
   subject { described_class.instance }
 
-  def test_class
-    Class.new do
-      extend NxtErrorRegistry
-    end
-  end
-
-  BadError = Class.new(StandardError)
-
   describe '#generate_code' do
     before do
       expect(SecureRandom).to receive(:uuid).and_return('5c8152cd-b8b9-4fb0-a5fe-5c11d200affc')
